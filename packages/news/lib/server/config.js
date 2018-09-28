74031d0eb7dfc6bc8457d5b803523cc44a825c40
lewi-g <dr.lewi@gmail.com>
2018-09-28 10:36:29 -0400

    reverting news package to Vulcan example forum

--- packages/news/lib/server/config.js
+++
@@ -1,9 +0,0 @@
-ServiceConfiguration.configurations.upsert(
-  { service: 'github' },
-  {
-    $set: {
-      clientId: Meteor.settings.oAuth.github.clientId,
-      secret: Meteor.settings.oAuth.github.secret
-    }
-  }
-)
